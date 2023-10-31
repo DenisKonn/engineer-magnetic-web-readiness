c60f35ca76146d8fd736f718caebc5f00b0a4124
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-10-31 16:08:33 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+def main():
+
+        print(f"Random Number: {item}")
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+import random
+    return data
+
+if __name__ == "__main__":
+
+
+    main()
